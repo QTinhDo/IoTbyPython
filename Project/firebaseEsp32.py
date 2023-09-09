a438301098fcbddc6df050f5fffe19e9926a8faf
QTinhDo <tinhdo150200@gmail.com>
2023-09-09 14:47:10 +0700

    commit

--- Project/firebaseEsp32.py
+++ Project/firebaseEsp32.py
@@ -1,7 +1,30 @@
 
 import requests as r
-from myPackage.speak_hear import *
-from myPackage.library import *
+from gtts import gTTS
+import playsound
+import os
+import speech_recognition as sr
+
+def speak(text):
+    print("Suti: " + text)
+    tts = gTTS(text=text, lang="vi", slow=False)              # Chuyển text thành âm thanh qua API Google
+    tts.save("./Project/sound.mp3")                           # Lưu lại âm thanh thành sound.mp3
+    playsound.playsound("./Project/sound.mp3", True)          # Phát file sound.mp3
+    os.remove("./Project/sound.mp3")                          # Xóa file sound.mp3
+
+
+def hear():
+    print("Đang nghe:.....")
+    r = sr.Recognizer() 
+    # source = sr.Microphone()
+    with sr.Microphone() as source:
+        audio = r.listen(source, phrase_time_limit= 3)  # Mở một micro mới với tên source sau khi thoát with as thì tắt micro đi
+        try:
+            text = r.recognize_google(audio, language="vi-VN")  # Lưu câu nói dưới dạng âm thanh (.mp3)
+            print("Tôi: " + text)
+            return str(text).lower()
+        except:
+            return None
 
 url = "https://esp32-remote-4fc72-default-rtdb.firebaseio.com/.json"
 
@@ -44,3 +67,7 @@ while True:
             json["yellow"] = 0    
             s = r.put(url = url, json = json)    
             speak("Led xanh và led vàng đã được tắt")
+
+    elif "tạm biệt" in you:
+        speak("Tạm biệt, hẹn gặp lại bạn sau")
+        break
