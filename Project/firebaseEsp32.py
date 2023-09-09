24b028ac90f53633efb50547140c2a351f26cb48
QTinhDo <tinhdo150200@gmail.com>
2023-09-09 14:41:40 +0700

    commit

--- Project/firebaseEsp32.py
+++ Project/firebaseEsp32.py
@@ -1,8 +1,46 @@
 
 import requests as r
+from myPackage.speak_hear import *
+from myPackage.library import *
 
 url = "https://esp32-remote-4fc72-default-rtdb.firebaseio.com/.json"
 
-content = r.get(url = url)
+json = {"green": 0, "yellow": 0}
 
+speak("Xin chào mình là trợ lý ảo Suti!!")
 
+while True:
+
+    you = hear()
+
+    if you == None:
+        speak("Mình chưa nghe bạn nói, bạn nói lại đi?")
+
+    elif "bật" in you:
+        if "xanh" in you:
+            json["green"] = 1
+            s = r.put(url = url, json = json)
+            speak("Led xanh đã được bật")
+        elif "vàng" in you:
+            json["yellow"] = 1
+            s = r.put(url = url, json = json)
+            speak("Led vàng đã được bật")
+        elif "hai" in you: 
+            json["green"] = 1    
+            json["yellow"] = 1    
+            s = r.put(url = url, json = json)    
+            speak("Led xanh và led vàng đã được mở")
+    elif "tắt" in you:
+        if "xanh" or "hai" in you:
+            json["green"] = 0
+            s = r.put(url = url, json = json)
+            speak("Led xanh đã được tắt")
+        elif "vàng" or "hai" in you:
+            json["yellow"] = 0
+            s = r.put(url = url, json = json)
+            speak("Led vàng đã được tắt")
+        elif "hai" in you: 
+            json["green"] = 0    
+            json["yellow"] = 0    
+            s = r.put(url = url, json = json)    
+            speak("Led xanh và led vàng đã được tắt")
